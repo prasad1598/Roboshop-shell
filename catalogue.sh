6816f3c28dafb81826c0926134ea006a197b5986
muniprasad <muniprasaddiddi@gmail.com>
2025-04-21 12:16:12 +0530

    shell files created

---
+++ catalogue.sh
@@ -0,0 +1,13 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+systemctl enable catalogue
+systemctl start catalogue
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
