1dcf57165016167b5dd05c6e3d27a4960b89bf31
muniprasad <muniprasaddiddi@gmail.com>
2025-04-21 13:17:08 +0530

    shell files created

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,10 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
+rm -rf /etc/nginx/nginx.conf
 cp nginx.conf /etc/nginx/nginx.conf
 rm -rf /usr/share/nginx/html/*
+rm -r /tmp/frontend.zip
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
