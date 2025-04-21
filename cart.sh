8b5da343f9903e85c425f336d47ddac74b4dd0db
muniprasad <muniprasaddiddi@gmail.com>
2025-04-21 17:21:26 +0530

    file updated

--- cart.sh
+++ cart.sh
@@ -5,8 +5,10 @@ cp cart.service vim /etc/systemd/system/cart.service
 
 useradd roboshop
 mkdir /app
+rm /tmp/*
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app
+rm /app/*
 unzip /tmp/cart.zip
 npm install
 systemctl daemon-reload
