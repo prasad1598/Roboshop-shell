cfb597870abec30ce105f9b9165a7e652c67e12d
muniprasad <muniprasaddiddi@gmail.com>
2025-04-21 12:57:27 +0530

    shell files created

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,4 @@
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 systemctl enable mongod
